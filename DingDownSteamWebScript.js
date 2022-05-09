3bf2e72a9142e8c95e8b391b324d2368cd40a9be
Julius <39952090+Smiorld@users.noreply.github.com>
2022-05-09 14:59:41 +0100

    fix match

--- DingDownSteamWebScript.js
+++ DingDownSteamWebScript.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         叮当公共库收录情况（测试）
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  在steam网页中浏览游戏页面时，在标题后追加显示其在叮当公共库的收录情况。
 // @author       Julius
-// @match        https://store.steampowered.com/app/*
+// @match        https://store.steampowered.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=atomicobject.com
 // @grant        GM_xmlhttpRequest
 // @connect      ruku.ga
