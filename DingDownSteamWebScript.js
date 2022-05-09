4146f8f0bd8302bc3d0ef2f571d4544f6c824953
Julius <39952090+Smiorld@users.noreply.github.com>
2022-05-09 17:17:13 +0100

    fix multiple search/pagination in search page

--- DingDownSteamWebScript.js
+++ DingDownSteamWebScript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         叮当公共库收录情况（测试）
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  在steam网页中浏览游戏页面时，在标题后追加显示其在叮当公共库的收录情况。
 // @author       Julius
 // @match        https://store.steampowered.com/*
@@ -11,9 +11,9 @@
 // @updateURL    https://github.com/Smiorld/DingDownSteamWebScript/blob/main/DingDownSteamWebScript.js
 // @downloadURL  https://github.com/Smiorld/DingDownSteamWebScript/blob/main/DingDownSteamWebScript.js
 // ==/UserScript==
-(function() {
-    'use strict';
 
+'use strict';
+window.addEventListener("load",function(){
     if(window.location=='https://store.steampowered.com/'){
         let tab_newreleases_content = document.querySelector('#tab_newreleases_content');//the box for searching result. each child in it is an <a>.
         let children = tab_newreleases_content.children;
@@ -105,4 +105,56 @@
             }
         }
     }
-})();
+})
+
+//mutation检测是否在搜索结果内部分有变化。若有，触发脚本
+if(window.location.pathname.split('/')[1]=='search'){
+    const targetNode = document.querySelector('#search_results');
+
+    const config = {
+        attributes: true,
+        childList: true,
+        characterData: true
+    };
+
+    const callback = mutations => {
+        mutations.forEach(mutation => {
+            if (mutation.type === 'childList') {
+                let searching_result = document.querySelector('#search_resultsRows');//the box for searching result. each child in it is an <a>.
+                let children = searching_result.children;
+                for(let i=0; i<children.length; i++){
+                    let child = children[i];
+                    if(child.href.split('/')[3]=='app'){
+
+                        let data = {Id: child.href.split('/')[4]};
+                        let title = child.children[1].children[0].children[0];
+                        GM_xmlhttpRequest ( {
+                            method:     "POST",
+                            url:        "https://ruku.ga/CheckId",
+                            data:       JSON.stringify(data),
+                            timeout:    20000,
+                            responseType:"json",
+                            ontimeout:  function (){
+                                console.log ("post request time out");
+                            },
+                            onload:     function (response) {
+                                console.log ("got response");
+                                console.log (response.response.Data);
+                                if (response.response.Data.Id == "0"){
+                                    title.innerHTML = "<span style='color:red;'>（未收录）</span>"+title.innerHTML;
+                                }
+                                else{
+                                    title.innerHTML = "<span style='color:green;'>（已收录）</span>"+title.innerHTML;
+                                }
+                            }
+                        } );
+                    }
+                }
+            }
+        });
+    }
+
+    const observer = new MutationObserver(callback);
+
+    observer.observe(targetNode, config);
+}
