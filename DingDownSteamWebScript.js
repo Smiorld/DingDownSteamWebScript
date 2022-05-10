f68a06cc3c57014be72d7cb7fe818c8d3d279ce7
Julius <39952090+Smiorld@users.noreply.github.com>
2022-05-10 16:12:50 +0100

    Update DingDownSteamWebScript.js

--- DingDownSteamWebScript.js
+++ DingDownSteamWebScript.js
@@ -9,8 +9,8 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=atomicobject.com
 // @grant        GM_xmlhttpRequest
 // @connect      ruku.ga
-// @updateURL    https://github.com/Smiorld/DingDownSteamWebScript/blob/master/DingDownSteamWebScript.js
-// @downloadURL  https://github.com/Smiorld/DingDownSteamWebScript/blob/master/DingDownSteamWebScript.js
+// @updateURL    https://github.com/Smiorld/DingDownSteamWebScript/blob/main/DingDownSteamWebScript.js
+// @downloadURL  https://github.com/Smiorld/DingDownSteamWebScript/blob/main/DingDownSteamWebScript.js
 // @license MIT
 // ==/UserScript==
 
