d9fb37c991abb7b087a192c4b819c5b322594b59
Julius <39952090+Smiorld@users.noreply.github.com>
2022-05-10 15:03:51 +0100

    Create DingDownSteamWebScript.meta.js

---
+++ DingDownSteamWebScript.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         叮当公共库收录情况（测试）
+// @namespace    http://tampermonkey.net/
+// @version      0.91
+// @description  在steam网页中浏览游戏页面时，在标题后追加显示其在叮当公共库的收录情况。
+// @author       Julius
+// @match        https://store.steampowered.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=atomicobject.com
+// @grant        GM_xmlhttpRequest
+// @connect      ruku.ga
+// @updateURL    https://github.com/Smiorld/DingDownSteamWebScript/blob/main/DingDownSteamWebScript.js
+// @downloadURL  https://github.com/Smiorld/DingDownSteamWebScript/blob/main/DingDownSteamWebScript.js
+// @license MIT
+// ==/UserScript==
