4045d29028a4bfa1d3008e5d13047c7d33a5befb
Julius <39952090+Smiorld@users.noreply.github.com>
2022-05-17 16:42:18 +0100

    fixed @connect

--- DingDownSteamWebScript.js
+++ DingDownSteamWebScript.js
@@ -2,13 +2,13 @@
 // @name         叮当公共库收录情况（测试）
 // @homepage     https://github.com/Smiorld/DingDownSteamWebScript
 // @namespace    https://github.com/Smiorld
-// @version      0.9.7
+// @version      0.9.8
 // @description  在steam网页中浏览游戏页面时，在标题后追加显示其在叮当公共库的收录情况。
 // @author       Smiorld
 // @match        https://store.steampowered.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=atomicobject.com
 // @grant        GM_xmlhttpRequest
-// @connect      ruku.ga
+// @connect      ddapi.133233.xyz
 // @updateURL    https://github.com/Smiorld/DingDownSteamWebScript/blob/main/DingDownSteamWebScript.js
 // @downloadURL  https://github.com/Smiorld/DingDownSteamWebScript/blob/main/DingDownSteamWebScript.js
 // @license MIT
