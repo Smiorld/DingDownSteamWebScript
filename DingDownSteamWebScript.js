f7c2bc59649b57b4247acc56accb1afa23e57136
Smiorld <zhouq13@cardiff.ac.uk>
2022-06-02 13:45:07 +0100

    version increment

--- DingDownSteamWebScript.js
+++ DingDownSteamWebScript.js
@@ -2,7 +2,7 @@
 // @name         叮当公共库收录情况（适配油猴tampermoneky与Steam++）
 // @homepage     https://github.com/Smiorld/DingDownSteamWebScript
 // @namespace    https://github.com/Smiorld
-// @version      1.0.13
+// @version      1.0.14
 // @description  在steam网页中浏览游戏页面时，在标题后追加显示其在叮当公共库的收录情况。
 // @author       Smiorld
 // @match        https://store.steampowered.com/*
